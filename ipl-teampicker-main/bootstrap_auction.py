73eab4b3045bced7274ee27ee32dc9adebebaf65
KJMvanderHorst <115348690+KJMvanderHorst@users.noreply.github.com>
2024-05-23 16:47:00 +0200

    added bootstrapping with improved sampling

--- ipl-teampicker-main/bootstrap_auction.py
+++ ipl-teampicker-main/bootstrap_auction.py
@@ -1,31 +1,37 @@
 import numpy as np
 import pandas as pd
-#this file containts one main function which you can call and it returns a list with resampled auction prices as a csv
 
-def resample_acution(auction : pd.DataFrame, resample : float) -> pd.DataFrame:
+def resample_acution(auction : pd.DataFrame, reshuffle : bool) -> pd.DataFrame:
     """
     This function takes the dataframe for an auction and resamples it. Resulting in a new auction.
+    It samples 3 groups seperately as they are distinct in price distribution.
+    - capped indian players
+    - uncapped indian players
+    - international
     Args:
         auction (pd.DataFrame): The original auction that we resample.
-        resample (float): The percent of the bids we resample.
-
+        reshuffle (bool): True if we want to change the order of players being sold for the new auction
     Returns:
-        pd.DataFrame
+        pd.DataFrame : the new auction
     """
-    prices = auction['Price'].to_list() #list of the prices
 
-    players = np.arange(len(auction))
+    indian_capped_price = auction[(auction['IndianOverseas'] == 'Indian') & (auction['Played For Country'] == 1)]['Selling Price'].tolist()
+    indian_uncapped_price = auction[(auction['IndianOverseas'] == 'Indian') & (auction['Played For Country'] == 0)]['Selling Price'].tolist()
+    international_price = auction[auction['IndianOverseas'] == 'Overseas']['Selling Price'].tolist()
 
-    n = np.floor(len(auction) * resample) # how many players price we resample
 
-    resample_prices = (np.random.choice(players, size = n, replace = False)) # index for the players whose price we reample
+    new_auction = auction.copy()
 
-    filtered_prices = [price for idx, price in enumerate(prices) if idx not in resample_prices]
+    new_auction['Selling Price'] = new_auction.apply(lambda row:new_price(row['IndianOverseas'], row['Played For Country']), axis=1)
 
-    sampled_prices = np.random.choice(filtered_prices, size = n, replace = True)
+    return new_auction
 
-    for i in range(n):
-        prices[resample_prices[i]] = sampled_prices[i]
-    
-    auction['Sample-Price'] = prices
-    return auction
+def new_price(nationality : str , capped : int) -> float:
+    global indian_capped_price, indian_uncapped_price, international_price
+
+    if nationality == 'Overseas':
+        return np.random.choice(international_price)
+    elif capped == 0:
+        return np.random.choice(indian_uncapped_price)
+    else:
+        return np.random.choice(indian_capped_price)
