e115e87c4022f38376065ef20ded2ad477660e31
Nateml <nateml.mac@gmail.com>
2024-06-24 23:34:41 +0200

    added second innings sim

--- t20simulation/match_simulator.py
+++ t20simulation/match_simulator.py
@@ -85,6 +85,8 @@ class MatchSimulator:
             if ball_number % 6  == 0:
                 on_strike, off_strike = off_strike, on_strike
 
+            ball_number += 1
+
         # Store the number of runs,wickets the batting team ended up with
         if team1_is_batting:
             self.team1_runs = runs
@@ -93,34 +95,87 @@ class MatchSimulator:
             self.team2_runs = runs
             self.team2_wickets = wickets
 
-    def calculate_over_needed_aggresivness(self):
+        return runs,wickets,ball_number
+
+    def simulate_inning_2(self, team2_is_batting, target_score):
+        wickets = 0
+        ball_number = 1
+        runs = 0
+
+        if team2_is_batting:
+            batting_order = self.team2
+        else:
+            batting_order = self.team1
+
+        on_strike = batting_order[0]
+        off_strike = batting_order[1]
+
+        while wickets < 10 and ball_number < 120 and runs < target_score:
+            over = (ball_number - 1) // 6
+
+            # Extra
+            if self.rng.random() < self.extra_prob:
+                runs += 1
+                continue
+
+            # Get the probabilities of each outcome
+            if over == 20:
+                pass
+            characteristic_over = self.calculate_over_needed_aggresivness(target_score, runs, over, wickets, on_strike)
+            p = self.helper.get_batting_probabilities(on_strike, characteristic_over, wickets)
+
+            # Get the outcome
+            outcome = self.rng.choice(8, p=p)
+
+            # Check if the outcome is a wicket
+            if outcome == 7:
+                wickets += 1
+                if wickets == 10:
+                    break
+                on_strike = batting_order[wickets+1] # New batter on strike
+            else:
+                runs += outcome
+
+            # If it is the last ball of the over, change who is on strike
+            if ball_number % 6  == 0:
+                on_strike, off_strike = off_strike, on_strike
+
+            ball_number += 1
+
+        # Store the number of runs,wickets the batting team ended up with
+        if team2_is_batting:
+            self.team2_runs = runs
+            self.team2_wickets = wickets
+        else:
+            self.team1_runs = runs
+            self.team1_wickets = wickets
+
+        return runs,wickets,ball_number
+
+    def calculate_over_needed_aggresivness(self, target_runs : int, current_runs : int, current_over : int, current_wickets : int, current_batter : int):
         #we calculate the over with needed aggressiveness for the team batting second for the current state of the game
         #assumes that the team batting second is the team with the lower score
         #returns the over number needed for the team batting second to win the match
-        ratio = (self.team1_runs - self.team2_runs + 1) / self.get_duckworth_lewis_table_value(self.current_over, self.team2_wickets)
-        for over in range(self.current_over, 20):
-            if ratio < self.__expected_runs(over) / self.__expected_resource_loss(over):
+        ratio = 0.8 * (target_runs - current_runs + 1) / self.get_duckworth_lewis_table_value(current_over, current_wickets)
+        for over in range(current_over, 19):
+            if ratio < self.__expected_runs(over, current_batter, current_wickets) / self.__expected_resource_loss(over, current_batter, current_wickets):
                 return over
-        return 20
-
-    def simulate_inning_2():
-
-        pass
+        return 19
 
     def get_duckworth_lewis_table_value(self, overs : int, wickets : int) -> float:
         return self.duckwort_lewis_table.iloc[overs, wickets]
 
-    def __expected_runs(self, over: int) -> float:
-        batting_probabilities = self.helper.get_batting_probabilities(self.current_batter, over, self.team2_wickets)
+    def __expected_runs(self, over: int, current_batter, wickets) -> float:
+        batting_probabilities = self.helper.get_batting_probabilities(current_batter, over, wickets)
         outcomes = np.arange(1, 7)
         expected_runs = np.sum(outcomes * batting_probabilities[1:7])
         return expected_runs
 
 
-    def __expected_resource_loss(self, over : int) -> float:
-        batting_probabilities = self.helper.get_batting_probabilities(self.current_batter, over, self.team2_wickets)
-        x = self.get_duckworth_lewis_table_value(over, self.team2_wickets) - self.get_duckworth_lewis_table_value(over + 1, self.team2_wickets)
-        y = self.get_duckworth_lewis_table_value(over, self.team2_wickets) - self.get_duckworth_lewis_table_value(over, self.team2_wickets + 1)
+    def __expected_resource_loss(self, over : int, current_batter, wickets) -> float:
+        batting_probabilities = self.helper.get_batting_probabilities(current_batter, over, wickets)
+        x = self.get_duckworth_lewis_table_value(over, wickets) - self.get_duckworth_lewis_table_value(over + 1, wickets)
+        y = self.get_duckworth_lewis_table_value(over, wickets) - self.get_duckworth_lewis_table_value(over, wickets + 1)
         expected_loss = x*np.sum(batting_probabilities[0:7]) + y*batting_probabilities[7]
         return expected_loss
         
@@ -129,8 +184,10 @@ def main():
     team1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
     team2 = np.array([12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22])
     sim = MatchSimulator(team1, team2)
-    sim.simulate_inning_1(True)
-    print(f"Team 1 score: {sim.team1_runs}/{sim.team1_wickets}")
+    target,team1_wickets,team1_deliveries = sim.simulate_inning_1(True)
+    chase,team2_wickets,team2_deliveries = sim.simulate_inning_2(True, target)
+    print(f"Team 1 score: {target}/{team1_wickets} in {team1_deliveries} balls")
+    print(f"Team 2 score: {chase}/{team2_wickets} in {team2_deliveries} balls")
 
 if __name__ == '__main__':
     main()
