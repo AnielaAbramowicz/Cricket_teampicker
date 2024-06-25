fcf5eb875bbf0c1a3b87e0a47973b4a1bf602404
Nateml <nateml.mac@gmail.com>
2024-06-25 21:38:01 +0200

    changed match sim experiments

--- t20simulation/match_sim_experiments.py
+++ t20simulation/match_sim_experiments.py
@@ -1,6 +1,7 @@
-from match_simulator import matchSimulator
 import pandas as pd
 import numpy as np
+import os
+from match_simulator import MatchSimulator
 
 
 #all_player.csv should have the following columns: player_id, player_type, player_value
@@ -8,53 +9,79 @@ import numpy as np
 class MatchSimulatorExperiment:
 
     def __init__(self, num_iterations : int) -> None:
-        self.player_data = pd.read_csv('ipl-teampicker-main/2024_auction_pool.csv') #TODO: replace with actual path
+        #self.player_data = pd.read_csv('ipl-teampicker-main/2024_auction_pool.csv') #TODO: replace with actual path
         self.rng = np.random.default_rng()
         self.num_iterations = num_iterations
 
-    def run_experiments(self, opponent_teams : np.ndarray[np.ndarrays[int]], our_team : np.ndarray_int) -> dict[np.ndarray[int], dict[np.ndarray[int], int]]:
+    def run_experiments(self, opponent_teams : np.ndarray[np.ndarray[np.ndarray[int]]], our_team : np.ndarray[np.ndarray[int]]) -> dict[np.ndarray[int], dict[np.ndarray[int], int]]:
         """
         This function runs the experiments for the match simulator
         param opponent_teams: np.ndarray[np.ndarray[int]]: The teams that we are playing against
         param our_team: np.ndarray[int]: The team that we are playing with
         """
-        results = {}
+        results = []
         for opponent_team in opponent_teams:
-            result = {our_team: 0, opponent_team: 0} #stores the number of wins for each team
-            for iteration in range(self.num_iterations):
+            result = [0, 0] #stores the number of wins for each team
 
-                selected_players = self.select_players(our_team)
-                selected_opponents =  self.select_players(opponent_team)
-                our_bowlers = selected_players[selected_players['player_type'] == 'Bowler' | selected_players['player_type'] == 'All-Rounder']
-                opponent_bowlers = selected_opponents[selected_opponents['player_type'] == 'Bowler' | selected_opponents['player_type'] == 'All-Rounder']
-                match = matchSimulator(selected_players, selected_opponents, our_bowlers, opponent_bowlers)
-                target, our_team_wickets, our_team_deliveries = match.simulate_inning_1(True)
-                chase, opponent_wickets,opponent_team_deliveries = match.simulate_inning_2(True, target)
-                if chase < target:
-                    result[our_team] += 2
-                elif chase > target:
-                    result[opponent_team] += 2
-                else :
-                    result[our_team] += 1
-                    result[opponent_team] += 1
-            for iteration in range(self.num_iterations):
-                selected_players = self.select_players(our_team)
-                selected_opponents =  self.select_players(opponent_team)
-                our_bowlers = selected_players[selected_players['player_type'] == 'Bowler' | selected_players['player_type'] == 'All-Rounder']
-                opponent_bowlers = selected_opponents[selected_opponents['player_type'] == 'Bowler' | selected_opponents['player_type'] == 'All-Rounder']
-                match = matchSimulator(selected_players, selected_opponents, our_bowlers, opponent_bowlers)
-                target, opponent_team_wickets, opponent_team_deliveries = match.simulate_inning_1(False)
-                chase, our_team_wickets, our_team_deliveries = match.simulate_inning_2(False, target)
-                if chase < target:
-                    result[our_team] += 2
-                elif chase > target:
-                    result[opponent_team] += 2
-                else :
-                    result[our_team] += 1
-                    result[opponent_team] += 1
-            results[opponent_team] = result
+            match = MatchSimulator(np.ones(11), np.ones(11))
 
-    def select_players(self, players : np.ndarary[int]) -> np.ndarray[int]:
+            # Simulate matches when our team bats first
+            for _ in range(self.num_iterations):
+
+                selected_team_batting = self.select_players(our_team[0])
+                selected_opponents_batting =  self.select_players(opponent_team[0])
+
+                selected_team_bowling = self.select_players(our_team[1])
+                selected_opponents_bowling =  self.select_players(opponent_team[1])
+
+                match.reset_with_new_teams(selected_team_batting, selected_opponents_batting, selected_team_bowling, selected_opponents_bowling)
+
+                target = match.simulate_inning_1(True)[0]
+                chase = match.simulate_inning_2(True, target)[1]
+
+                if chase < target:
+                    result[0] += 1
+                elif chase > target:
+                    result[1] += 1
+                else :
+                    result[0] += 0.5
+                    result[1] += 0.5
+
+            # Simulate matches when our team bats second
+            for _ in range(self.num_iterations):
+                selected_team_batting = self.select_players(our_team[0])
+                selected_opponents_batting =  self.select_players(opponent_team[0])
+
+                selected_team_bowling = self.select_players(our_team[1])
+                selected_opponents_bowling =  self.select_players(opponent_team[1])
+
+                match.reset_with_new_teams(selected_team_batting, selected_opponents_batting, selected_team_bowling, selected_opponents_bowling)
+
+                target = match.simulate_inning_1(False)[0]
+                chase = match.simulate_inning_2(False, target)[0]
+
+                if chase < target:
+                    result[0] += 1
+                elif chase > target:
+                    result[1] += 1
+                else :
+                    result[0] += 0.5
+                    result[1] += 0.5
+
+            results.append(result)
+
+        return results
+
+    def select_players(self, players : np.ndarray[int]) -> np.ndarray[int]:
+        """
+        This function selects the playing 11 for the match from the 25 players available in a team.
+        It does so randomly.
+        """
+
+        # Randomly select 11 players
+        return self.rng.choice(players, size=11, replace=False)
+
+    def select_players_old(self, players : np.ndarray[int]) -> np.ndarray[int]:
         """
         This function selects the playing 11 for the match from the 25 players available in a team
         by drawing from the distribution of evaluations of the players.
@@ -83,6 +110,27 @@ class MatchSimulatorExperiment:
 
         return np.concatenate((sampled_bowlers, sampled_players))
 
+def main():
+    our_team_batting_ids = np.array([-1, 399, 548, 483, 590, 97, 476, 3046, 423, 389, 1890, 368, -1, 346, -1, 340, -1, -1, -1, 544, -1, -1, -1, -1])
+    out_team_bowling_ids = np.array([-1, 301, -1, -1, 456, -1, -1, -1, 299, 291, 427, 174, -1, -1, -1, -1, -1, -1, -1, 235, -1, -1, -1, -1])
+    our_team = np.array([our_team_batting_ids, out_team_bowling_ids])
+
+    # Read in the opponent teams
+    path = os.path.dirname(os.path.realpath(__file__))
+    squads = pd.read_csv(os.path.join(path, 'IPL_2024_Player_Squads.csv'))
+    teams = squads['TEAM'].unique()
+    opponent_teams = []
+    for i, t in enumerate(teams):
+        batting_ids = squads[squads['TEAM'] == t]['batting id'].values
+        bowling_ids = squads[squads['TEAM'] == t]['bowling id'].values
+        opponent_teams.append(np.array([batting_ids, bowling_ids]))
+
+    match_simulator = MatchSimulatorExperiment(100)
+    results = match_simulator.run_experiments(opponent_teams, our_team)
+    print(results)
+
+if __name__ == '__main__':
+    main()
     
 
 
@@ -91,4 +139,7 @@ class MatchSimulatorExperiment:
         
 
     
+
+
+
     
--- t20simulation/match_simulator.py
+++ t20simulation/match_simulator.py
@@ -48,10 +48,40 @@ class MatchSimulator:
         self.helper.initialize()
         print('Done.')
 
+    def reset_with_new_teams(self, team1 : np.ndarray[int], team2: np.ndarray[int], team1_bowling_lineup : np.ndarray[int] = np.array([-1,-1,-1,-1,-1]), team2_bowling_lineup : np.ndarray[int] = np.array([-1,-1,-1,-1,-1])):
+        """
+        This function resets the match simulator with new teams.
+
+        param team1: The first team playing the match, represented as an array of batting ids
+        param team2: The second team playing the match, reprersented as an array of batting ids
+
+        Both team arrays need to be of length 11.
+        """
+
+        assert len(team1) == 11, f'Team 1 has to contain exactly 11 players, but it contains {len(team1)}'
+        assert len(team2) == 11, f'Team 2 has to contain exactly 11 players, but it contains {len(team2)}'
+
+        self.team1 = team1
+        self.team2 = team2
+
+        self.team1_runs = 0
+        self.team1_wickets = 0
+
+        self.team2_runs = 0
+        self.team2_wickets = 0
+
+        self.current_batter = None
+
+        self.team1_bowling_lineup = team1_bowling_lineup
+        self.team2_bowling_lineup = team2_bowling_lineup
+
     def simulate_inning_1(self, team1_is_batting : bool):
         wickets = 0
         ball_number = 1
         runs = 0
+
+        run_history = np.array([0]*120) # These keep track of the runs and wickets at each ball
+        wicket_history = np.array([0]*120)
 
         bowler_counter = np.array([0]*len(self.team1_bowling_lineup))
         bowlers = self.team1_bowling_lineup.copy()
@@ -72,6 +102,7 @@ class MatchSimulator:
             # Extra
             if self.rng.random() < self.extra_prob:
                 runs += 1
+                run_history[ball_number-1] += 1
                 continue
 
             # Get the probabilities of each outcome
@@ -83,11 +114,13 @@ class MatchSimulator:
             # Check if the outcome is a wicket
             if outcome == 7:
                 wickets += 1
+                wicket_history[ball_number-1] = 1
                 if wickets == 10:
                     break
                 on_strike = batting_order[wickets+1] # New batter on strike
             else:
                 runs += outcome
+                run_history[ball_number-1] += outcome
 
             # If it is the last ball of the over, change who is on strike and change the bowler
             if ball_number % 6  == 0:
@@ -109,12 +142,15 @@ class MatchSimulator:
             self.team2_runs = runs
             self.team2_wickets = wickets
 
-        return runs,wickets,ball_number
+        return runs,wickets,ball_number,run_history,wicket_history
 
     def simulate_inning_2(self, team2_is_batting, target_score):
         wickets = 0
         ball_number = 1
         runs = 0
+
+        run_history = np.array([0]*120) # These keep track of the runs and wickets at each ball
+        wicket_history = np.array([0]*120)
 
         bowler_counter = np.array([0]*len(self.team2_bowling_lineup))
         bowlers = self.team2_bowling_lineup.copy()
@@ -135,6 +171,7 @@ class MatchSimulator:
             # Extra
             if self.rng.random() < self.extra_prob:
                 runs += 1
+                run_history[ball_number-1] += 1
                 continue
 
             # Get the probabilities of each outcome
@@ -149,11 +186,13 @@ class MatchSimulator:
             # Check if the outcome is a wicket
             if outcome == 7:
                 wickets += 1
+                wicket_history[ball_number-1] = 1
                 if wickets == 10:
                     break
                 on_strike = batting_order[wickets+1] # New batter on strike
             else:
                 runs += outcome
+                run_history[ball_number-1] += outcome
 
             # If it is the last ball of the over, change who is on strike
             if ball_number % 6  == 0:
@@ -175,13 +214,13 @@ class MatchSimulator:
             self.team1_runs = runs
             self.team1_wickets = wickets
 
-        return runs,wickets,ball_number
+        return runs,wickets,ball_number,run_history,wicket_history
 
     def calculate_over_needed_aggresivness(self, target_runs : int, current_runs : int, current_over : int, current_wickets : int, current_batter : int):
         #we calculate the over with needed aggressiveness for the team batting second for the current state of the game
         #assumes that the team batting second is the team with the lower score
         #returns the over number needed for the team batting second to win the match
-        ratio = 0.8 * (target_runs - current_runs + 1) / self.get_duckworth_lewis_table_value(current_over, current_wickets)
+        ratio = 0.6 * (target_runs - current_runs + 1) / self.get_duckworth_lewis_table_value(current_over, current_wickets)
         for over in range(current_over, 19):
             if ratio < self.__expected_runs(over, current_batter, current_wickets) / self.__expected_resource_loss(over, current_batter, current_wickets):
                 return over
@@ -205,14 +244,63 @@ class MatchSimulator:
         return expected_loss
         
 
+def get_run_distribution(team1, team2, num_iterations):
+    """
+    This function simulates a match between team1 and team2 for num_iterations and returns the total number of runs scored in the first innings of all the games.
+    """
+
+    runs = []
+
+    for i in range(num_iterations):
+        sim = MatchSimulator(team1, team2)
+
+        target = sim.simulate_inning_1(True)[0]
+
+        runs.append(target)
+
+    return np.array(runs)
+
+def run_standard_match_experiment():
+    team1 = np.full(11, -1)
+    team2 = np.full(11, -1)
+
+    n = 1
+    total_run_history_team1 = np.zeros(120)
+    total_wicket_history_team1 = np.zeros(120)
+    total_run_history_team2 = np.zeros(120)
+    total_wicket_history_team2 = np.zeros(120)
+
+    for i in range(n):
+        sim = MatchSimulator(team1, team2)
+        target,team1_wickets,team1_deliveries,run_history_team1,wicket_history_team1 = sim.simulate_inning_1(True)
+        chase,team2_wickets,team2_deliveries,run_history_team2,wicket_history_team2 = sim.simulate_inning_2(True, target)
+
+        total_run_history_team1 += run_history_team1
+        total_wicket_history_team1 += wicket_history_team1
+        total_run_history_team2 += run_history_team2
+        total_wicket_history_team2 += wicket_history_team2
+
+    run_history_team1 = total_run_history_team1 / n
+    wicket_history_team1 = total_wicket_history_team1 / n
+    run_history_team2 = total_run_history_team2 / n
+    wicket_history_team2 = total_wicket_history_team2 / n
+
+    # Save to csv
+    results = pd.DataFrame(columns=['ball', 'team1_runs', 'team1_wickets', 'team2_runs', 'team2_wickets'])
+    results['ball'] = np.arange(1, 121)
+    results['team1_runs'] = run_history_team1
+    results['team1_wickets'] = wicket_history_team1
+    results['team2_runs'] = run_history_team2
+    results['team2_wickets'] = wicket_history_team2
+    results.to_csv(os.path.join(MatchSimulator.path, 'avg_match_progression.csv'), index=False)
+
 def main():
-    team1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
-    team2 = np.array([12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22])
-    sim = MatchSimulator(team1, team2)
-    target,team1_wickets,team1_deliveries = sim.simulate_inning_1(True)
-    chase,team2_wickets,team2_deliveries = sim.simulate_inning_2(True, target)
-    print(f"Team 1 score: {target}/{team1_wickets} in {team1_deliveries} balls")
-    print(f"Team 2 score: {chase}/{team2_wickets} in {team2_deliveries} balls")
+    team1 = np.full(11, -1)
+    team2 = np.full(11, -1)
+    run_distribution = get_run_distribution(team1, team2, 100)
+    # Save to file
+    pd.DataFrame(run_distribution).to_csv(os.path.join(MatchSimulator.path, 'first_innings_run_distribution.csv'), index=False)
+
 
 if __name__ == '__main__':
     main()
